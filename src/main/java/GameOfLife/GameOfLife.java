c84f085506884420a9b33ec63a38ef5ccefa5f6c
toffe <toffe950301@gmail.com>
2020-10-26 19:51:42 +0100

    Added test to check gridsize and code to make it pass

--- src/main/java/GameOfLife/GameOfLife.java
+++ src/main/java/GameOfLife/GameOfLife.java
@@ -2,6 +2,20 @@ package GameOfLife;
 
 public class GameOfLife {
 
+    private final int height;
+    private final int width;
 
+    GameOfLife(int height, int width, Cell[] aliveCells) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int height() {
+        return height;
+    }
+
+    public int width() {
+        return width;
+    }
 
 }
--- src/test/java/GameOfLife/GameOfLifeTest.java
+++ src/test/java/GameOfLife/GameOfLifeTest.java
@@ -1,15 +1,38 @@
 package GameOfLife;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
 
+    GameOfLife gameOfLife;
+    private final Cell[] aliveCells = {
+            new Cell(4,4),
+            new Cell(4,5),
+            new Cell(4,6),
+            new Cell(1,1),
+            new Cell(8,7)
+    };
+
+    @BeforeEach
+    void initEach() {
+        gameOfLife = new GameOfLife(10,10, aliveCells);
+    }
+
     @Test
     void InitializeGameOfLifeObjectExpectingNotNull() {
-        GameOfLife gameOfLife = new GameOfLife();
+        GameOfLife gameOfLife = new GameOfLife(10,10, aliveCells);
         assertNotNull(gameOfLife);
     }
 
+    @Test
+    void CheckHeightAndWidthOfGridExpectingTenAndTen() {
+        assertEquals(10, gameOfLife.height());
+        assertEquals(10, gameOfLife.width());
+    }
 }
