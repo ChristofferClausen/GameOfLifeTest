df390dbf9a4cf0756a07d02d14a8221df7a6b560
toffe <toffe950301@gmail.com>
2020-10-27 22:45:51 +0100

    Added some methods to run the program from the main class

--- src/main/java/GameOfLife/Main.java
+++ src/main/java/GameOfLife/Main.java
@@ -1,5 +1,7 @@
 package GameOfLife;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -7,32 +9,55 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+
     public static void main(String[] args) {
-        GameOfLife gameOfLife = new GameOfLife(10,10);
-        List<Position> positions;
-        positions = aliveCells();
-        gameOfLife.initializeFirstGeneration(positions);
-        var grid = gameOfLife.getGrid();
-        loopGenerations(gameOfLife, grid);
+        var height = Integer.parseInt(args[0]);
+        var width = Integer.parseInt(args[1]);
+        var gameOfLife = initializeGame(height,width,args[2]);
+        loopGenerations(gameOfLife);
     }
 
-    private static List<Position> aliveCells() {
-        List<Position> positions;
-        positions = new ArrayList<>();
-        positions.add(new Position(0,0));
-        positions.add(new Position(4, 4));
-        positions.add(new Position(4, 5));
-        positions.add(new Position(4, 6));
-        positions.add(new Position(4, 1));
-        positions.add(new Position(8, 8));
+    private static GameOfLife initializeGame(int height, int width, String pattern) {
+        GameOfLife gameOfLife = new GameOfLife(height,width);
+        gameOfLife.initializeFirstGeneration(pattern(pattern));
+        System.out.println(gameOfLife.gridString());
+        return gameOfLife;
+    }
+
+    private static List<Position> pattern(String pattern) {
+        switch (pattern) {
+            case "glider":
+                return glider();
+            case "oscillator":
+                return oscillator();
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private static void loopGenerations(GameOfLife gameOfLife) {
+        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(() -> {
+            gameOfLife.nextGeneration(gameOfLife.getGrid());
+            System.out.println(gameOfLife.gridString());
+        }, 0, 500, TimeUnit.MILLISECONDS);
+    }
+
+    private static List<Position> oscillator() {
+        List<Position> positions = new ArrayList<>();
+        positions.add(new Position(1, 0));
+        positions.add(new Position(1, 1));
+        positions.add(new Position(1, 2));
         return positions;
     }
 
-    private static void loopGenerations(GameOfLife gameOfLife, Cell[][] grid) {
-        final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.scheduleAtFixedRate(() -> {
-            gameOfLife.nextGeneration(grid);
-            System.out.println(gameOfLife.gridString());
-        }, 0, 500, TimeUnit.MILLISECONDS);
+    private static List<Position> glider() {
+        List<Position> positions = new ArrayList<>();
+        positions.add(new Position(0,2));
+        positions.add(new Position(1, 0));
+        positions.add(new Position(1, 2));
+        positions.add(new Position(2, 1));
+        positions.add(new Position(2, 2));
+        return positions;
     }
 }
